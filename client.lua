87ca54462f715fc872fd63610e261bd915972235
Jared <TheWolfBadger@gmail.com>
2020-06-23 12:57:03 -0400

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,69 @@
+-------------------------
+--- Badger-Priorities ---
+-------------------------
+
+function Draw2DText(x, y, text, scale)
+    -- Draw text on screen
+    SetTextFont(4)
+    SetTextProportional(7)
+    SetTextScale(scale, scale)
+    SetTextColour(255, 255, 255, 255)
+    SetTextDropShadow(0, 0, 0, 0,255)
+    SetTextDropShadow()
+    SetTextEdge(4, 0, 0, 0, 255)
+    SetTextOutline()
+    SetTextJustification(0)
+    SetTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawText(x, y)
+end
+drawText = Config.Options.CooldownDisplay:gsub("{MINS}", "0");
+RegisterNetEvent('Badger-Priorities:DrawText')
+AddEventHandler('Badger-Priorities:DrawText', function(text)
+	drawText = text;
+end)
+drawFast = false;
+RegisterNetEvent('Badger-Priorities:TriggerTrue')
+AddEventHandler('Badger-Priorities:TriggerTrue', function()
+	drawFast = true 
+end)
+displayCooldown = 0;
+Citizen.CreateThread(function()
+	while true do 
+		Wait(0);
+		local x = Config.DisplayLocation.x;
+		local y = Config.DisplayLocation.y;
+		if #drawText > 1 then 
+			Draw2DText(x, y, drawText, 0.5);
+		end 
+		if drawFast then 
+			Draw2DText(.5, .5, Config.Options.TooFastDisplay, 1.0)
+		end
+		if Config.Options.EnableSpeedMessage then 
+			local ped = GetPlayerPed(-1);
+			local veh = GetVehiclePedIsIn(ped, 0);
+			if veh ~= nil then 
+				-- They are in a vehicle 
+				if GetVehicleClass(veh) ~= 14 and GetVehicleClass(veh) ~= 15 and GetVehicleClass(veh) ~= 16 then 
+					-- It's a car 
+					local speed = GetEntitySpeed(veh) * 2.236936;
+					if speed >= 80 then 
+						-- Too fast if we are on cooldown
+						if displayCooldown <= 2000 then 
+							TriggerServerEvent('Badger-Priorities:TriggerCheck')
+						else 
+							drawFast = false;
+						end
+						displayCooldown = displayCooldown + 1;
+						if displayCooldown == (1000 * 60) then 
+							displayCooldown = 0;
+						end
+					else 
+						drawFast = false;
+						displayCooldown = 0;
+					end
+				end
+			end
+		end
+	end
+end)
---
+++ server.lua
@@ -0,0 +1,134 @@
+-------------------------
+--- Badger-Priorities ---
+-------------------------
+
+function msg(src, text)
+  -- Send message 
+  TriggerClientEvent('chatMessage', src, Config.prefix .. text);
+end
+
+RegisterNetEvent('Badger-Priorities:TriggerCheck')
+AddEventHandler('Badger-Priorities:TriggerCheck', function()
+  local src = source;
+  if inProgess or onHold or (currentCooldownTime > 0) then 
+    TriggerClientEvent('Badger-Priorities:TriggerTrue', src)
+  end
+end)
+
+RegisterCommand("resetpcd", function(source, args, rawCommand)
+  -- /resetpcd
+  local src = source;
+  if IsPlayerAceAllowed(src, "Badger-Priorities.ResetPCD") or IsPlayerAceAllowed(src, "Badger-Priorities.Commands") then
+    onHold = false;
+    inProgress = false;
+    currentCooldownTime = 0;
+    local display = Config.Options.CooldownDisplay:gsub("{MINS}", currentCooldownTime);
+    TriggerClientEvent('Badger-Priorities:DrawText', -1, display)
+    msg(src, 'You have reset the priority cooldown...');
+  end
+end)
+RegisterCommand("inprogress", function(source, args, rawCommand)
+  -- /inprogress 
+  local src = source;
+  if IsPlayerAceAllowed(src, "Badger-Priorities.InProgress") or IsPlayerAceAllowed(src, "Badger-Priorities.Commands") then
+    onHold = false;
+    inProgress = true;
+    currentCooldownTime = 0;
+    --msg(src, 'There is a priority in progress...');
+    TriggerClientEvent('Badger-Priorities:DrawText', -1, Config.Options.InProgressDisplay:gsub("{MINS}", currentCooldownTime))
+    msg(-1, Config.Messages.InProgressMessage) 
+  end
+end)
+RegisterCommand("onhold", function(source, args, rawCommand)
+  -- /onhold 
+  local src = source;
+  if IsPlayerAceAllowed(src, "Badger-Priorities.OnHold") or IsPlayerAceAllowed(src, "Badger-Priorities.Commands") then
+    inProgress = false;
+    onHold = true  
+    currentCooldownTime = 0;
+    --msg(src, 'The priorities have been set on hold...');
+    TriggerClientEvent('Badger-Priorities:DrawText', -1, Config.Options.OnHoldDisplay:gsub("{MINS}", currentCooldownTime))
+    msg(-1, Config.Messages.OnHoldMessage)
+  end
+end)
+RegisterCommand("cooldown", function(source, args, rawCommand)
+  -- /cooldown <time>
+  local src = source;
+  if IsPlayerAceAllowed(src, "Badger-Priorities.Cooldown") or IsPlayerAceAllowed(src, "Badger-Priorities.Commands") then 
+    if #args > 0 then 
+      -- Check if number 
+      if tonumber(args[1]) ~= nil then 
+        local coold = tonumber(args[1]);
+        currentCooldownTime = coold;
+        inProgress = false;
+        onHold = false;
+        --msg(src, 'The cooldown has been set to ^7' .. args[1] .. ' ^3minutes...');
+        local display = Config.Options.CooldownDisplay:gsub("{MINS}", currentCooldownTime);
+        TriggerClientEvent('Badger-Priorities:DrawText', -1, display)
+        msg(-1, Config.Messages.CooldownMessage:gsub("{MINS}", args[1]));
+      else 
+        -- Not a number 
+        msg(src, '^1ERROR: That is not a valid number');
+      end
+    else 
+      -- You need an argument length over 0
+      msg(src, '^1ERROR: The command usage is /cooldown <time>');
+    end
+  end 
+end)
+
+currentCooldownTime = 0;
+onHold = false;
+inProgress = false 
+
+Citizen.CreateThread(function()
+  while true do 
+    Wait((1000 * 5));
+    if not onHold and not inProgress then 
+      if Config.Options.AlwaysDisplay then 
+        local display = Config.Options.CooldownDisplay:gsub("{MINS}", currentCooldownTime);
+        TriggerClientEvent('Badger-Priorities:DrawText', -1, display);
+      else
+        if currentCooldownTime <= 0 then 
+          local display = Config.Options.CooldownDisplay:gsub("{MINS}", currentCooldownTime);
+          TriggerClientEvent('Badger-Priorities:DrawText', -1, "");
+        else 
+          local display = Config.Options.CooldownDisplay:gsub("{MINS}", currentCooldownTime);
+          TriggerClientEvent('Badger-Priorities:DrawText', -1, display);
+        end
+      end
+    end 
+    if onHold and not inProgress then  
+      -- Print on screen, the priorities are on hold 
+      TriggerClientEvent('Badger-Priorities:DrawText', -1, Config.Options.OnHoldDisplay:gsub("{MINS}", currentCooldownTime))
+    end 
+    if inProgress then 
+      TriggerClientEvent('Badger-Priorities:DrawText', -1, Config.Options.InProgressDisplay:gsub("{MINS}", currentCooldownTime))
+    end 
+  end
+end)
+
+Citizen.CreateThread(function()
+  while true do 
+    Wait((1000 * 60));
+    -- Every 60 seconds 
+    if not onHold and not inProgress then 
+      -- Print on screen, the cooldown time 
+      if Config.Options.AlwaysDisplay then 
+        local display = Config.Options.CooldownDisplay:gsub("{MINS}", currentCooldownTime);
+        TriggerClientEvent('Badger-Priorities:DrawText', -1, display);
+      else
+        if currentCooldownTime <= 0 then 
+          local display = Config.Options.CooldownDisplay:gsub("{MINS}", currentCooldownTime);
+          TriggerClientEvent('Badger-Priorities:DrawText', -1, "");
+        else 
+          local display = Config.Options.CooldownDisplay:gsub("{MINS}", currentCooldownTime);
+          TriggerClientEvent('Badger-Priorities:DrawText', -1, display);
+        end
+      end
+      if currentCooldownTime > 0 then 
+        currentCooldownTime = currentCooldownTime - 1;
+      end
+    end 
+  end
+end)
