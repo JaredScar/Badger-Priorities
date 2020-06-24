dbe671241f6841209bf5c6cb1aa10250324b1152
Jared <TheWolfBadger@gmail.com>
2020-06-23 20:23:15 -0400

    Add files via upload

--- server.lua
+++ server.lua
@@ -18,30 +18,32 @@ end)
 RegisterCommand("resetpcd", function(source, args, rawCommand)
   -- /resetpcd
   local src = source;
-  if src <= 0 or IsPlayerAceAllowed(src, "Badger-Priorities.ResetPCD") or IsPlayerAceAllowed(src, "Badger-Priorities.Commands") then
+  if IsPlayerAceAllowed(src, "Badger-Priorities.ResetPCD") or IsPlayerAceAllowed(src, "Badger-Priorities.Commands") or src <= 0 then
     onHold = false;
     inProgress = false;
+	resetPCD = true;
     currentCooldownTime = 0;
-    local display = Config.Options.CooldownDisplay:gsub("{MINS}", currentCooldownTime);
-    TriggerClientEvent('Badger-Priorities:DrawText', -1, display)
-    if src > 0 then 
-      msg(src, 'You have reset the priority cooldown...');
-    else 
-      print("You have reset the priority cooldown...");
+    TriggerClientEvent('Badger-Priorities:DrawText', -1, Config.Options.resetDisplay:gsub("{MINS}", currentCooldownTime))
+	if Config.Options.EnablePCDMessage then -- Reset PCD Message Toggle
+		msg(-1, Config.Messages.resetMessage) 
+    elseif src <= 0 then 
+      print(Config.Messages.resetMessage)
     end
   end
 end)
 RegisterCommand("inprogress", function(source, args, rawCommand)
   -- /inprogress 
   local src = source;
-  if src <= 0 or IsPlayerAceAllowed(src, "Badger-Priorities.InProgress") or IsPlayerAceAllowed(src, "Badger-Priorities.Commands") then
+  if IsPlayerAceAllowed(src, "Badger-Priorities.InProgress") or IsPlayerAceAllowed(src, "Badger-Priorities.Commands") or src <= 0 then
     onHold = false;
     inProgress = true;
+	resetPCD = false;
     currentCooldownTime = 0;
     --msg(src, 'There is a priority in progress...');
     TriggerClientEvent('Badger-Priorities:DrawText', -1, Config.Options.InProgressDisplay:gsub("{MINS}", currentCooldownTime))
-    msg(-1, Config.Messages.InProgressMessage) 
-    if src <= 0 then 
+	if Config.Options.EnableProgressMessage then -- inProgress Message Toggle
+		msg(-1, Config.Messages.InProgressMessage) 
+    elseif src <= 0 then 
       print(Config.Messages.InProgressMessage)
     end
   end
@@ -49,14 +51,16 @@ end)
 RegisterCommand("onhold", function(source, args, rawCommand)
   -- /onhold 
   local src = source;
-  if src <= 0 or IsPlayerAceAllowed(src, "Badger-Priorities.OnHold") or IsPlayerAceAllowed(src, "Badger-Priorities.Commands") then
+  if IsPlayerAceAllowed(src, "Badger-Priorities.OnHold") or IsPlayerAceAllowed(src, "Badger-Priorities.Commands") or src <= 0 then
     inProgress = false;
-    onHold = true  
+    onHold = true;
+	resetPCD = false;	
     currentCooldownTime = 0;
     --msg(src, 'The priorities have been set on hold...');
     TriggerClientEvent('Badger-Priorities:DrawText', -1, Config.Options.OnHoldDisplay:gsub("{MINS}", currentCooldownTime))
-    msg(-1, Config.Messages.OnHoldMessage)
-    if src <= 0 then 
+	if Config.Options.EnableHoldMessage then -- On Hold Message Toggle
+	    msg(-1, Config.Messages.OnHoldMessage)
+    elseif src <= 0 then 
       print(Config.Messages.OnHoldMessage)
     end
   end
@@ -64,7 +68,7 @@ end)
 RegisterCommand("cooldown", function(source, args, rawCommand)
   -- /cooldown <time>
   local src = source;
-  if src <= 0 or IsPlayerAceAllowed(src, "Badger-Priorities.Cooldown") or IsPlayerAceAllowed(src, "Badger-Priorities.Commands") then 
+  if IsPlayerAceAllowed(src, "Badger-Priorities.Cooldown") or IsPlayerAceAllowed(src, "Badger-Priorities.Commands") or src <= 0 then 
     if #args > 0 then 
       -- Check if number 
       if tonumber(args[1]) ~= nil then 
@@ -72,15 +76,13 @@ RegisterCommand("cooldown", function(source, args, rawCommand)
         currentCooldownTime = coold;
         inProgress = false;
         onHold = false;
+		resetPCD = false;
         --msg(src, 'The cooldown has been set to ^7' .. args[1] .. ' ^3minutes...');
         local display = Config.Options.CooldownDisplay:gsub("{MINS}", currentCooldownTime);
         TriggerClientEvent('Badger-Priorities:DrawText', -1, display)
-        if src > 0 then 
-          msg(-1, Config.Messages.CooldownMessage:gsub("{MINS}", args[1]));
-        else 
-          msg(-1, Config.Messages.CooldownMessage:gsub("{MINS}", args[1]));
-          print(Config.Messages.CooldownMessage:gsub("{MINS}", args[1]))
-        end
+		if Config.Options.EnableCooldownMessage then -- Cooldown Message Toggle
+			msg(-1, Config.Messages.CooldownMessage:gsub("{MINS}", args[1]))
+		end
       else 
         -- Not a number 
         msg(src, '^1ERROR: That is not a valid number');
@@ -95,11 +97,12 @@ end)
 currentCooldownTime = 0;
 onHold = false;
 inProgress = false 
+resetPCD = false
 
 Citizen.CreateThread(function()
   while true do 
     Wait((1000 * 5));
-    if not onHold and not inProgress then 
+    if not onHold and not inProgress and not resetPCD then 
       if Config.Options.AlwaysDisplay then 
         local display = Config.Options.CooldownDisplay:gsub("{MINS}", currentCooldownTime);
         TriggerClientEvent('Badger-Priorities:DrawText', -1, display);
@@ -113,12 +116,15 @@ Citizen.CreateThread(function()
         end
       end
     end 
-    if onHold and not inProgress then  
+    if onHold and not inProgress and not resetPCD then  
       -- Print on screen, the priorities are on hold 
       TriggerClientEvent('Badger-Priorities:DrawText', -1, Config.Options.OnHoldDisplay:gsub("{MINS}", currentCooldownTime))
     end 
     if inProgress then 
       TriggerClientEvent('Badger-Priorities:DrawText', -1, Config.Options.InProgressDisplay:gsub("{MINS}", currentCooldownTime))
+	end
+	if resetPCD then
+	  TriggerClientEvent('Badger-Priorities:DrawText', -1, Config.Options.resetDisplay:gsub("{MINS}", currentCooldownTime))
     end 
   end
 end)
@@ -127,7 +133,7 @@ Citizen.CreateThread(function()
   while true do 
     Wait((1000 * 60));
     -- Every 60 seconds 
-    if not onHold and not inProgress then 
+    if not onHold and not inProgress and not resetPCD then 
       -- Print on screen, the cooldown time 
       if Config.Options.AlwaysDisplay then 
         local display = Config.Options.CooldownDisplay:gsub("{MINS}", currentCooldownTime);
