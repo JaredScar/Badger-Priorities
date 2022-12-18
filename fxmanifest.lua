468a8d120bafa57bc5405d7f515d539502a5c2bf
Nick Reagan <52389274+NickReagan@users.noreply.github.com>
2022-12-17 21:25:09 -0500

    Updated to fxmanifest.

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+author "Badger#0002"
+description "Badger Priorites"
+fx_version "cerulean"
+game "gta5"
+
+client_script "client.lua"
+
+server_script "server.lua"
+
+shared_scripts {
+    "config.lua",
+}
